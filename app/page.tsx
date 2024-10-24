d29c670e3c9e8880c9bf6599b98996523a0d5159
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-10-24 14:34:50 +0430

    Set-up the nextui

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from '@nextui-org/react';
 export default function Home() {
-  return <div>Hello Next</div>;
+  return (
+    <div>
+      Here is the buttton
+      <Button>Hello</Button>
+    </div>
+  );
 }
